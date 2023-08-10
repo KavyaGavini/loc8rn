18a6b7dd063d35d7dfab75e835380515071b0073
KavyaGavini <kavyagoud163@gmail.com>
2023-08-10 15:54:00 +0530

    initial commit

--- app_server/controller/locations.js
+++ app_server/controller/locations.js
@@ -1,18 +1,17 @@
-/* GET 'home' page */
-const homelist = (req, res) => {
-    res.render('index', { title: 'Home' });
-   };
-   /* GET 'Location info' page */
-   const locationInfo = (req, res) => {
-    res.render('index', { title: 'Location info' });
-   };
-   /* GET 'Add review' page */
-   const addReview = (req, res) => {
-    res.render('index', { title: 'Add review' });
-   };
-
-module.exports = {
-    homelist,
-    locationInfo,
-    addReview
-   };
+module.exports.homelist=function(req,res){
+    res.render('locations-list',{title:'Home'});
+};
+/* get location info page */
+module.exports.locationInfo=function(req,res){
+    res.render('location-info',{title:'Location Info'});
+};
+module.exports.locationInfo2=function(req,res){
+    res.render('location-info2',{title:'Location Info'});
+};
+module.exports.locationInfo3=function(req,res){
+    res.render('location-info3',{title:'Location Info'});
+};
+/*  get add review page */
+module.exports.addReview=function(req,res){
+    res.render('location-review-form',{title:'Add Review'});
+};
